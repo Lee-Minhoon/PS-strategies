5981b24350771d8e3d714d1bafe9d25258c80d3f
Lee Minhoon <lmh_1220@naver.com>
2020-12-28 04:19:48 +0900

    init commit

---
+++ PS-strategies/소스.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+using namespace std;
+
+double PrintCost(int* cost, int n, int l)
+{
+    double avg, res = 1000000;
+
+    for (int i = l; i <= n; i++) {
+        for (int j = 0; j <= n - i; j++) {
+            avg = 0;
+            for (int k = j; k < j + i; k++) {
+                avg += cost[k];
+            }
+            avg /= i;
+            if (avg < res) {
+                res = avg;
+            }
+        }
+    }
+
+    return res;
+}
+
+int main(void)
+{
+    int c; cin >> c;
+    for (int i = 0; i < c; i++) {
+        int n, l; cin >> n >> l;
+        int* cost = new int[n];
+        for (int j = 0; j < n; j++) {
+            cin >> cost[j];
+        }
+        cout << PrintCost(cost, n, l) << endl;
+    }
+
+    return 0;
+}
