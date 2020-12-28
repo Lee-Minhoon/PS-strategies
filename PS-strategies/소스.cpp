f044fc0bd12d0c813fc00570913e3164f3069dfa
Lee Minhoon <lmh_1220@naver.com>
2020-12-29 08:04:00 +0900

    init commit

--- PS-strategies/소스.cpp
+++ PS-strategies/소스.cpp
@@ -1,38 +1,29 @@
-#include<iostream>
-
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+#include<algorithm>
 using namespace std;
+int C, N, L, n[1001];
+int main() {
+	for (scanf("%d", &C); C--;) {
+		scanf("%d %d", &N, &L);
+		for (int i = 1; i <= N; i++) scanf("%d", &n[i]), n[i] += n[i - 1];
+		for (int i = 1; i <= N; i++) {
+			printf("%d", n[i]);
+		}
+		double ans = 1e9;
+		// N-L -> 0 1 2 3
+		for (int i = 0; i <= N - L; i++)
+			// 3 4 5 6 부터 6보다 작을때까지
+			for (int j = i + L; j <= N; j++)
+				ans = min(ans, double(n[j] - n[i]) / (j - i));
+		// n[3] - n[0] / 3
+		// n[4] - n[0] / 4
+		// n[5] - n[0] / 5
+		// n[6] - n[0] / 6
 
-double PrintCost(int* cost, int n, int l)
-{
-    double avg, res = 1000000;
-
-    for (int i = l; i <= n; i++) {
-        for (int j = 0; j <= n - i; j++) {
-            avg = 0;
-            for (int k = j; k < j + i; k++) {
-                avg += cost[k];
-            }
-            avg /= i;
-            if (avg < res) {
-                res = avg;
-            }
-        }
-    }
-
-    return res;
-}
-
-int main(void)
-{
-    int c; cin >> c;
-    for (int i = 0; i < c; i++) {
-        int n, l; cin >> n >> l;
-        int* cost = new int[n];
-        for (int j = 0; j < n; j++) {
-            cin >> cost[j];
-        }
-        cout << PrintCost(cost, n, l) << endl;
-    }
-
-    return 0;
+		// n[4] - n[1] / 3
+		// n[5] - n[1] / 4
+		printf("%.11f\n", ans);
+	}
+	return 0;
 }
