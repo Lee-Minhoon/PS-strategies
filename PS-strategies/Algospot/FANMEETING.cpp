1d594d6001abfc88544886cef53874c55cf50d8a
Lee Minhoon <hackids@naver.com>
2021-03-31 04:27:26 +0900

    init commit

--- PS-strategies/Algospot/FANMEETING.cpp
+++ PS-strategies/Algospot/FANMEETING.cpp
@@ -3,44 +3,45 @@
 
 using namespace std;
 
+using sz = size_t;
+
 const int MAX = 200000;
 
-void addTo(vector<int>& a, const vector<int>& b, int k) {
-    int an = a.size(), bn = b.size();
+void addTo(vector<int>& a, const vector<int>& b, sz k) {
+    sz an = a.size(), bn = b.size();
     a.resize(max(an, bn + k));
-    for (int i = 0; i < bn; i++) {
+    for (sz i = 0; i < bn; i++) {
         a[i + k] += b[i];
     }
 }
 
 void subFrom(vector<int>& a, const vector<int>& b) {
-    int an = a.size(), bn = b.size();
-    a.resize(max(an, bn + 1));
-    for (int i = 0; i < bn; i++) {
+    sz an = a.size(), bn = b.size();
+    a.resize(max(an, bn));
+    for (sz i = 0; i < bn; i++) {
         a[i] -= b[i];
     }
 }
 
 vector<int> multiply(const vector<int>& a, const vector<int>& b) {
-    int an = a.size(), bn = b.size();
-    vector<int> ret(an + bn + 1, 0);
-    for (int i = 0; i < an; i++) {
-        for (int j = 0; j < bn; j++) {
+    sz an = a.size(), bn = b.size();
+    vector<int> ret(an + bn - 1, 0);
+    for (sz i = 0; i < an; i++)
+        for (sz j = 0; j < bn; j++)
             ret[i + j] += (a[i] * b[j]);
-        }
-    }
+
     return ret;
 }
 
 vector<int> karatsuba(const vector<int>& a, const vector<int>& b)
 {
-    int an = a.size(), bn = b.size();
+    sz an = a.size(), bn = b.size();
     
     if (an < bn) return karatsuba(b, a);
     if (an == 0 || bn == 0) return vector<int>();
     if (an <= 50) return multiply(a, b);
 
-    int half = an / 2;
+    sz half = an / 2;
     vector<int> a0(a.begin(), a.begin() + half);
     vector<int> b0(b.begin(), b.begin() + min(half, bn));
     vector<int> a1(a.begin() + half, a.end());
@@ -65,18 +66,19 @@ vector<int> karatsuba(const vector<int>& a, const vector<int>& b)
 
 int hugs(const string& members, const string &fans)
 {
-    int N = members.size(), M = fans.size();
+    sz N = members.size(), M = fans.size();
     vector<int> A(N), B(M);
-    for (int i = 0; i < N; i++) {
+    for (sz i = 0; i < N; i++) {
         A[i] = (members[i] == 'M');
     }
-    for (int i = 0; i < M; i++) {
+    for (sz i = 0; i < M; i++) {
         B[M - i - 1] = (fans[i] == 'M');
     }
 
     int ret = 0;
+
     vector<int> res = karatsuba(B, A);
-    for (int i = N - 1; i < M; i++) {
+    for (sz i = N - 1; i < M; i++) {
         if(res[i] == 0) ret++;
     }
 
@@ -85,6 +87,10 @@ int hugs(const string& members, const string &fans)
 
 int main(void)
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int c; cin >> c;
 
     for (int i = 0; i < c; i++) {
--- PS-strategies/test.cpp
+++ PS-strategies/test.cpp
@@ -1,109 +1,107 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
-#define endl '\n'
-#define fastio cin.sync_with_stdio(false); cin.tie(nullptr)
+#include<iostream>
+#include<vector>
 
 using namespace std;
 
-//O(n^2) 곱셈 알고리즘
-vector<int> multiply(const vector<int>& a, const vector<int>& b) {
-    vector<int> c(a.size() + b.size() + 1, 0);
-    for (int i = 0; i < a.size(); i++)
-        for (int j = 0; j < b.size(); j++)
-            c[i + j] += (a[i] * b[j]);
-    return c;
-}
+using sz = size_t;
 
-//a += b*(10^k)
-void addTo(vector<int>& a, const vector<int>& b, int k) {
-    a.resize(max(a.size(), b.size() + k));
-    for (int i = 0; i < b.size(); i++)
+const int MAX = 200000;
+
+void addTo(vector<int>& a, const vector<int>& b, sz k) {
+    sz an = a.size(), bn = b.size();
+    a.resize(max(an, bn + k));
+    for (sz i = 0; i < bn; i++) {
         a[i + k] += b[i];
+    }
 }
 
-//a -= b
 void subFrom(vector<int>& a, const vector<int>& b) {
-    a.resize(max(a.size(), b.size()) + 1);
-    for (int i = 0; i < b.size(); i++)
+    sz an = a.size(), bn = b.size();
+    a.resize(max(an, bn));
+    for (sz i = 0; i < bn; i++) {
         a[i] -= b[i];
+    }
 }
 
-vector<int> karatsuba(const vector<int>& a, const vector<int>& b) {
-    int an = a.size(), bn = b.size();
-    if (an < bn)
-        return karatsuba(b, a);
-    if (an == 0 || bn == 0)
-        return vector<int>();
-    //크기가 작은경우 카라츠바 알고리즘을 사용하지 않고 구한다.
-    if (an <= 50)
-        return multiply(a, b);
+vector<int> multiply(const vector<int>& a, const vector<int>& b) {
+    sz an = a.size(), bn = b.size();
+    vector<int> ret(an + bn - 1, 0);
+    for (sz i = 0; i < an; i++)
+        for (sz j = 0; j < bn; j++)
+            ret[i + j] += (a[i] * b[j]);
 
-    /*카라츠바 알고리즘
-    ∴ z0 + ( z1 * 10^half ) + ( z2 * 10^(half*2) )
-        z0 = a0 * b0
-        z2 = a1 * b1
-        z1 = (a0 + b1) * (b0 + b1) - z0 - z2
-        a0 = a 앞부분 절반 b0 = b 앞부분 절반
-        a1 = a 뒷부분 절반 b1 = b 뒷부분 절반
-    */
+    return ret;
+}
 
-    //a와 b를 절반으로 나눈다.
-    int half = an / 2;
+vector<int> karatsuba(const vector<int>& a, const vector<int>& b)
+{
+    sz an = a.size(), bn = b.size();
+
+    if (an < bn) return karatsuba(b, a);
+    if (an == 0 || bn == 0) return vector<int>();
+    if (an <= 50) return multiply(a, b);
+
+    sz half = an / 2;
     vector<int> a0(a.begin(), a.begin() + half);
+    vector<int> b0(b.begin(), b.begin() + min(half, bn));
     vector<int> a1(a.begin() + half, a.end());
-    vector<int> b0(b.begin(), b.begin() + min<int>(bn, half));
-    vector<int> b1(b.begin() + min<int>(bn, half), b.end());
+    vector<int> b1(b.begin() + min(half, bn), b.end());
 
     vector<int> z2 = karatsuba(a1, b1);
     vector<int> z0 = karatsuba(a0, b0);
 
     addTo(a0, a1, 0);
     addTo(b0, b1, 0);
-
     vector<int> z1 = karatsuba(a0, b0);
     subFrom(z1, z0);
     subFrom(z1, z2);
 
-    vector<int> res;
-    addTo(res, z0, 0);
-    addTo(res, z1, half);
-    addTo(res, z2, half * 2);
+    vector<int> ret;
+    addTo(ret, z0, 0);
+    addTo(ret, z1, half);
+    addTo(ret, z2, half * 2);
 
-    return res;
+    return ret;
 }
 
-int hugs(const string& members, const string& fans) {
-    int N = members.size(), M = fans.size();
+int hugs(const string& members, const string& fans)
+{
+    sz N = members.size(), M = fans.size();
     vector<int> A(N), B(M);
-    for (int i = 0; i < N; i++)
+    for (sz i = 0; i < N; i++) {
         A[i] = (members[i] == 'M');
-    for (int i = 0; i < M; i++)
+    }
+    for (sz i = 0; i < M; i++) {
         B[M - i - 1] = (fans[i] == 'M');
+    }
 
-    vector<int> C = karatsuba(A, B);
-    int allHugs = 0;
+    int ret = 0;
 
-    //모든 멤버들이 펜을 만나기 시작한 시점부터
-    for (int i = 0; i < C.size(); i++)
-    {
-        cout << C[i] << ' ';
+    vector<int> res = karatsuba(B, A);
+    for (int i = 0; i < res.size(); i++) {
+        cout << res[i] << ' ';
     }
     cout << endl;
-    return allHugs;
+    for (sz i = N - 1; i < M; i++) {
+        if (res[i] == 0) ret++;
+    }
+
+    return ret;
 }
 
-int main() {
-    fastio;
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    int _;
-    cin >> _;
-    while (_--) {
+    int c; cin >> c;
+
+    for (int i = 0; i < c; i++) {
         string members, fans;
         cin >> members >> fans;
         cout << hugs(members, fans) << endl;
     }
+
     return 0;
 }
